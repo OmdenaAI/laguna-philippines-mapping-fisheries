3f96a83feb434b2bacd811469203b29366ce0400
Paolo Dano <paolodano1@gmail.com>
2022-12-10 13:59:48 +0800

    Added Code for Recommendation Generation

---
+++ src/tasks/task-3-recommendation-engine/subtask-3-tabular-consolidation/config.py
@@ -0,0 +1,5 @@
+import os
+
+IMG_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', 'Satellite Images', 'Processed Images')
+IMG_FULL_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', 'Satellite Images', 'Satellite Images')
+TAB_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'Tabular Data')
---
+++ src/tasks/task-3-recommendation-engine/subtask-4-recommendation/config.py
@@ -0,0 +1,7 @@
+import os
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data')
+
+IMG_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', 'Satellite Images', 'Processed Images')
+IMG_FULL_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', 'Satellite Images', 'Satellite Images')
+TAB_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'Tabular Data')
