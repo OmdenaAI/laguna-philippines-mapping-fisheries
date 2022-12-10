e4d5e9d4b9e77ceab516fa4e1ffc126ea316370b
jplaulau14 <code.patricklaurel@gmail.com>
2022-12-10 22:24:03 +0800

    added filtered geojson

--- src/tasks/task-1-web-app/app.py
+++ src/tasks/task-1-web-app/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import json
+import requests
 
 st.set_page_config(layout="wide")
 
@@ -13,9 +15,9 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[16.879721, 121.774017], zoom=8)
-        cities = r'C:\Users\patrick\Documents\Omdena\Challenges\Laguna Local Chapter\laguna-philippines-mapping-fisheries\src\data\Cities\ph_cities.csv'
-        regions = r"https://github.com/OmdenaAI/laguna-philippines-mapping-fisheries/blob/main/src/data/Results/region_2_results.geojson"
-        
+        cities = 'https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Cities/ph_cities.csv'
+        regions = 'https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Results/region_2_results.geojson'
+
         m.add_geojson(regions, layer_name='Thingy')
         m.add_points_from_xy(
             cities,
@@ -27,3 +29,18 @@ with st.expander("See source code"):
             add_legend=True,
         )
 m.to_streamlit(height=700)
+
+geojson_url = 'https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Results/region_2_results.geojson'
+response = requests.get(geojson_url)
+geojson_data = response.json()
+
+geojson_dict = json.loads(geojson_data)
+
+
+def filter_properties(feature):
+  if 'total_area' in feature['properties'] and 'mean_temperature' in feature['properties']:
+    return True
+  else:
+    return False
+
+print(geojson_dict)
