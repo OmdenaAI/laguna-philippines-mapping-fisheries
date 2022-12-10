bd87833d8a2b79b79d18e5ab81246e1cab2c9ad5
jplaulau14 <code.patricklaurel@gmail.com>
2022-12-10 20:38:08 +0800

    added cities data

--- src/tasks/task-1-web-app/app.py
+++ src/tasks/task-1-web-app/app.py
@@ -14,8 +14,8 @@ with st.expander("See source code"):
 
         m = leafmap.Map(center=[16.879721, 121.774017], zoom=8)
         cities = r'C:\Users\patrick\Documents\Omdena\Challenges\Laguna Local Chapter\laguna-philippines-mapping-fisheries\src\data\Cities\ph_cities.csv'
-        regions = r"C:\Users\patrick\Documents\Omdena\Challenges\Laguna Local Chapter\laguna-philippines-mapping-fisheries\src\data\Results\region_2_results.geojson"
-
+        regions = r"https://github.com/OmdenaAI/laguna-philippines-mapping-fisheries/blob/main/src/data/Results/region_2_results.geojson"
+        
         m.add_geojson(regions, layer_name='Thingy')
         m.add_points_from_xy(
             cities,
