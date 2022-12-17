96b6a7a9574e8471b1077d9c41646c10210d27ba
kherzieandal <kherzieandal@gmail.com>
2022-12-17 15:02:23 +0800

    Update app.py
    - Added on click instead of hover
    - Replaced some markdown texts and texts in sidebar.

--- src/tasks/task-1-web-app/app.py
+++ src/tasks/task-1-web-app/app.py
@@ -5,24 +5,34 @@ st.set_page_config(layout="wide")
 
 st.sidebar.title("About")
 
-st.title("Marker Cluster")
+st.sidebar.title("Contact")
+st.sidebar.info(
+    '''
+    This project is owned by:\n
+    © Omdena Laguna, Philippines\n
+    omdena.laguna@gmail.com
+    '''
+)
 
-with st.expander("See source code"):
-    with st.echo():
+st.title("Map of Fishing Activity Recommendation")
 
-        m = leafmap.Map(center=[17.879721, 121.774017], zoom=7)
-        # cities = 'https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Cities/ph_cities.csv'
-        cities = "https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Cities/ph_cities_filtered.csv"
-        regions = 'https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Cities/region_2_results_filtered.geojson'
+with st.expander("How to use this map "):
+    st.markdown('''### Instructions:
+    1. ''')
 
-        m.add_geojson(regions, layer_name='Cluster Map')
-        m.add_points_from_xy(
-            cities,
-            x="lng",
-            y="lat",
-            # color_column='region',
-            icon_names=['gear', 'map', 'leaf', 'globe'],
-            spin=True,
-            add_legend=True,
-        )
+m = leafmap.Map(center=[17.879721, 121.774017], zoom=7)
+# cities = 'https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Cities/ph_cities.csv'
+cities = "https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Cities/ph_cities_filtered.csv"
+regions = 'https://raw.githubusercontent.com/OmdenaAI/laguna-philippines-mapping-fisheries/main/src/data/Cities/region_2_results_filtered.geojson'
+
+m.add_geojson(regions, layer_name='Cluster Map', info_mode="on_click")
+m.add_points_from_xy(
+    cities,
+    x="lng",
+    y="lat",
+    # color_column='region',
+    icon_names=['gear', 'map', 'leaf', 'globe'],
+    spin=True,
+    add_legend=True,
+)
 m.to_streamlit(height=700)
